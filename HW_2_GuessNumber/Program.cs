602379f9f84ef6cc7a6fb35d995e3927cb180878
Sergey Mykhailov <moderya7@gmail.com>
2019-03-06 14:09:11 +0200

    Update Program.cs
    comments

--- HW_2_GuessNumber/Program.cs
+++ HW_2_GuessNumber/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+using System;
 
 namespace HW_2_GuessNumber
 {
@@ -15,13 +14,22 @@ namespace HW_2_GuessNumber
             Console.ReadKey();
         }
 
+        // TODO:
+        // Если есть возвращаемое значение, то лучше переименовать метод, на что-то вроде "GetRandomNumber". 
+        // Сейчас по контексту названия выглядит будто инициализируется какое-то поле с типом "Random"
         private static int RandomInitialization()
         {
             Random random = new Random();
+            // TODO:
+            // 0 и 100 стоит вынести в параметры метода. любые подобные штуки лучше выносить в параметры
             int rndnum = random.Next(0, 100);
+            // TODO:
+            // https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/naming-parameters - код конвеншен по названию переменных, rndNum
             return rndnum;
         }
 
+        // TODO:
+        // Не совсем понятно зачем здесь возвращаемом значение
         private static bool NumberChecking(bool win, int rndnum)
         {
             while (win == false)
@@ -42,6 +50,12 @@ namespace HW_2_GuessNumber
                     win = false;
                     Console.WriteLine("Your number is less than mine. Try again");
                 }
+                // TODO:
+                // Все обьявленные конструкции if - else if - else выполняются в том порядке в котором они были обьявлены. 
+                // Соответственно сейчас последний else if никогда не отрабатывает, поскольку при вводе, к примеру, 
+                // 101 сработает просто  "else if (inputnum > rndnum)".
+                // Вот эту проверку на вообще допустимое значение введеного числа стоит сделать первым if, 
+                // проверять в нем на ((inputnum < minValue)||(inputnum > maxValue)), а minValue и maxValue сделать параметрами метода
                 else if (inputnum > 100)
                 {
                     win = false;
